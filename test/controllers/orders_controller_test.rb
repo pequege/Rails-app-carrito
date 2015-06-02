550677ccd10ee279308911d47a66d030dc443d60
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-02 17:01:22 -0300

    Added order form

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -5,6 +5,23 @@ class OrdersControllerTest < ActionController::TestCase
     @order = orders(:one)
   end
 
+  test "requires item in cart" do
+        get :new
+        assert_redirected_to store_path
+        assert_equal flash[:notice], 'Your cart is empty'
+    end
+
+  test "should get new" do
+    item = LineItem.new
+    item.build_cart
+    item.product = products(:ruby)
+    item.save!
+    session[:cart_id] = item.cart.id
+    get :new
+    assert_response :success
+  end
+
+
   test "should get index" do
     get :index
     assert_response :success
