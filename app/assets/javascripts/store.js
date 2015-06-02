b83399d3b6f7e4cc6aaf0cdf02abfd6ce3222ff5
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-02 16:12:04 -0300

    implemented Ajax for cart controller

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -1,2 +1,8 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+// $(document).on("ready, page:change", function(){
+// 	$('.store .entry > img').click(function(){
+// 		$(this).parent().find(':submit').click();
+// 	});
+// });
