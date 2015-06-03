7133660b8a0432b166b39cb6b34d12032e883a00
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-03 16:03:48 -0300

    fixed mailer and loggin in process

--- app/mailers/order_notifier.rb
+++ app/mailers/order_notifier.rb
@@ -5,10 +5,15 @@ class OrderNotifier < ApplicationMailer
   #
   #   en.order_notifier.received.subject
   #
-  def received
-    @greeting = "Hi"
+  def received(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Confirmation'
+  end
 
-    mail to: "to@example.org", subject: "hola"
+
+  def shipped(order)
+    @order = order
+    mail to: order.email, subject: 'Pragmatic Store Order Shipped'
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
