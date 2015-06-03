b9531975ffb4a75b8fbf6bdcb26b67ff3a3255ab
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-03 12:09:28 -0300

    atom feeds implemented

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   resources :line_items
   resources :carts
   get 'store/index'
+  resources :products do
+    get :who_bought, on: :member
+  end
+
 
   resources :products
   # The priority is based upon order of creation: first created -> highest priority.
