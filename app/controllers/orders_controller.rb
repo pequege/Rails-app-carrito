c65a14bd60c13468396755f50af495c0fe4c4cdb
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-04 16:48:26 -0300

    implemented spanish translation

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   skip_before_action :authorize, only: [:new, :create]
-  
+
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
 
@@ -42,8 +42,7 @@ class OrdersController < ApplicationController
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
         OrderNotifier.received(@order).deliver
-        format.html { redirect_to store_url, notice: 
-          'Thank you for your order.' }
+        format.html { redirect_to store_url, notice: I18n.t('.thanks') }
         format.json { render action: 'show', status: :created,
           location: @order }
       
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@
 #---
 Depot::Application.routes.draw do
   get 'admin' => 'admin#index'
+
   controller :sessions do
     get  'login' => :new
     post 'login' => :create
@@ -16,21 +17,24 @@ Depot::Application.routes.draw do
 
   get "sessions/create"
   get "sessions/destroy"
-  resources :users
-  resources :orders
-  resources :line_items
-  resources :carts
 
-  get "store/index"
+  resources :users
+
   resources :products do
     get :who_bought, on: :member
   end
 
+  scope '(:locale)' do
+    resources :orders
+    resources :line_items
+    resources :carts
+    root 'store#index', as: 'store', via: :all
+  end
+  
   # The priority is based upon order of creation:
   # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   # You can have the root of your site routed with "root"
-  root 'store#index', as: 'store'
   # ...
 
   # Example of regular route:
