7fb1987a8add96d2359ff20aa5b133865bbe47f1
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-16 11:35:53 -0300

    added validation for user_tutorial form

--- app/controllers/user_tutorials_controller.rb
+++ app/controllers/user_tutorials_controller.rb
@@ -11,15 +11,36 @@ class UserTutorialsController < ApplicationController
       if @order.save
         redirect_to tutorials_path, notice: 'Gracias por su compra'
       else
-        render action: 'new'
+        redirect_to buy_user_tutorials_path(order_params[:tutorial_id]), notice: @order.errors.full_messages.first
       end
     else
       redirect_to user_session_path, notice: 'Debes estar loggueado para poder comprar'
     end
   end
 
+  # def create
+  #   @order = Order.new(order_params)
+  #   @order.add_line_items_from_cart(@cart)
+
+  #   respond_to do |format|
+  #     if @order.save
+  #       Cart.destroy(session[:cart_id])
+  #       session[:cart_id] = nil
+  #       OrderNotifier.received(@order).deliver
+  #       format.html { redirect_to store_url, notice: I18n.t('.thanks') }
+  #       format.json { render action: 'show', status: :created,
+  #         location: @order }
+      
+  #     else
+  #       format.html { render action: 'new' }
+  #       format.json { render json: @order.errors,
+  #         status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
+
 private
   def order_params
-    params.require(:user_tutorial).permit(:name, :pay_type, :tutorial_id)
+    params.require(:user_tutorial).permit(:name, :pay_type, :tutorial_id, :email, :address)
   end
 end
--- app/models/user_tutorial.rb
+++ app/models/user_tutorial.rb
@@ -1,4 +1,7 @@
 class UserTutorial < ActiveRecord::Base
   belongs_to :user
   belongs_to :tutorial
+
+  validates :name, :address, :email, presence: true
+
 end
