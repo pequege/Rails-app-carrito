2abc971610ea78c6854724da26df9bfece56134c
Gerardo Mansilla <gerardoemansilla@gmail.com>
2015-06-16 16:46:24 -0300

    disable mailer

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -41,7 +41,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
-        OrderNotifier.received(@order).deliver
+        #OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: I18n.t('.thanks') }
         format.json { render action: 'show', status: :created,
           location: @order }
